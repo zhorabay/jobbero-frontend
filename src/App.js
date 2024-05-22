6b62d91605abadf9f6f09dbef9d5d7dba6f1fcb8
Assem Zhorabay <assem.zhorabay@gmail.com>
2024-05-22 12:47:48 +0300

    Add a route "/choose-a-course"

--- src/App.js
+++ src/App.js
@@ -67,6 +67,7 @@ const App = () => {
               <Route path="/categories/:categoryId/courses/:courseId/modules/:courseModuleId/post-lesson" element={<PostLesson />} />
               <Route path="/payment" element={<Payment />} />
               <Route path="/registration" element={<Registration />} />
+              <Route path="/choose-a-course" element={<ChooseCourse />} />
             </>
           ) : (
             <>
