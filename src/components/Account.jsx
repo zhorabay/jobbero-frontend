f6f959412100cb002b1688befea4746e9c23d92e
Assem Zhorabay <assem.zhorabay@gmail.com>
2024-06-14 16:20:51 +0300

    Update Profile page

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,26 +1,33 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchUserCourses } from '../redux/actions/userActions';
+import { fetchCourses } from '../redux/actions/courseActions';
 import down from '../media/down.png';
 import signin from '../media/sign-blue.png';
 import '../styles/Auth.css';
 import Navigation3 from './Navigation3';
 
-function Account() {
+function Account({ categoryId }) {
   const dispatch = useDispatch();
   const [isExpanded, setIsExpanded] = useState(true);
   const signedInUser = useSelector((state) => state.auth.user);
-  const userCourses = useSelector((state) => state.user.courses);
+  const userCourses = useSelector((state) => state.user.userCourses);
 
   useEffect(() => {
-    dispatch(fetchUserCourses(signedInUser.id));
-  }, [dispatch, signedInUser.id]);
+    if (categoryId) {
+      dispatch(fetchCourses(categoryId));
+    }
+  }, [dispatch, categoryId]);
+
+  useEffect(() => {
+    dispatch(fetchUserCourses(signedInUser?.id));
+  }, [dispatch, signedInUser]);
 
   const toggleExpand = () => {
     setIsExpanded(!isExpanded);
   };
-
-  const paidCourses = userCourses.filter((course) => course.modules.some((module) => module.payment_status === 'paid'));
 
   return (
     <>
@@ -31,13 +38,13 @@ function Account() {
             <h2 className="account-h2">Welcome to Origin8Lab Africa, Here is your dashboard</h2>
             <div className="account-section2">
               <div>
-                <img src={signedInUser.photo || signin} alt="Profile" className="account-signin" />
-                <p className="account-user">{signedInUser.name}</p>
+                <img src={signedInUser?.photo || signin} alt="Profile" className="account-signin" />
+                <p className="account-user">{signedInUser?.name}</p>
               </div>
             </div>
             <div className="account-section21">
               <div className="account-text">
-                <h3 className="account-h3">{paidCourses.length}</h3>
+                <h3 className="account-h3">{userCourses.length}</h3>
                 <p className="account-p">Courses</p>
               </div>
               <hr className="account-hr" />
@@ -65,19 +72,11 @@ function Account() {
             </button>
             {isExpanded && (
               <div className="account-list">
-                {paidCourses.map((course) => (
+                {userCourses && userCourses.map((course) => (
                   <div key={course.id} className="account-course">
-                    <h4 className="account-h4">{course.title}</h4>
-                    {course.modules.map((module) => (
-                      <div key={module.id} className="account-module">
-                        <h5 className="account-h5">{module.title}</h5>
-                        <ul>
-                          {module.lessons.map((lesson) => (
-                            <li key={lesson.id}>{lesson.title}</li>
-                          ))}
-                        </ul>
-                      </div>
-                    ))}
+                    <Link to={`/categories/${course.category_id}/courses/${course.id}/modules`} className="profile-h4">
+                      {course.title}
+                    </Link>
                   </div>
                 ))}
               </div>
@@ -88,5 +87,9 @@ function Account() {
     </>
   );
 }
+
+Account.propTypes = {
+  categoryId: PropTypes.string.isRequired,
+};
 
 export default Account;
